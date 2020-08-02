086fb5c95870a01d131c98a4f181f27c389c778b
Euphoria Yan <ysq58000@foxmail.com>
2020-08-02 12:58:34 +0800

    update

---
+++ scripts/gulian_ner_bert_ckpt_outputfile.sh
@@ -0,0 +1,15 @@
+
+
+python bin/run_ckpt_bert_tagger.py \
+--data_sign gulian_ner \
+--config_path configs/traditional_bert.json \
+--data_dir dataset/gulian_txt \
+--bert_model bert_daizhige \
+--use_crf \
+--task_name ner \
+--max_seq_length 128 \
+--do_eval \
+--test_batch_size 64 \
+--output_dir output \
+--ckpt_name gulian_ner_bert_daizhige.bin \
+--output_file \
