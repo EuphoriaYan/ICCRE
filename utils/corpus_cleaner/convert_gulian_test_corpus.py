a418addceb61cddc023a73bfd1ed986edd203580
Euphoria Yan <ysq58000@foxmail.com>
2020-09-22 18:39:55 +0800

    update

--- utils/corpus_cleaner/convert_gulian_test_corpus.py
+++ utils/corpus_cleaner/convert_gulian_test_corpus.py
@@ -15,58 +15,60 @@ import textwrap
 
 MAX_SEQ_LEN=126
 
-def main():
-    with open('dataset/gulian_txt/命名实体识别测试集922.txt', 'r', encoding='utf-8') as f:
-        lines = f.readlines()
 
-    def split_long_sent(line):
-        new_lines = []
-        new_line = ''
-        pieces = re.split(r'([，。！？：、‘’“”])', line.strip())
-        for piece in pieces:
-            if new_line != '' and len(new_line) + len(piece) > MAX_SEQ_LEN: # 超出长度，塞到list里
-                while len(new_line) > MAX_SEQ_LEN:
-                    new_lines.append(new_line[:MAX_SEQ_LEN])
-                    new_line = new_line[MAX_SEQ_LEN:]
-                if new_line != '':
-                    new_lines.append(new_line)
-                new_line = ''
-            else:
-                new_line += piece
-
-        if new_line != '':
+def split_long_sent(line):
+    new_lines = []
+    new_line = ''
+    pieces = re.split(r'([，。！？：、‘’“”])', line.strip())
+    for piece in pieces:
+        if new_line != '' and len(new_line) + len(piece) > MAX_SEQ_LEN:  # 超出长度，塞到list里
             while len(new_line) > MAX_SEQ_LEN:
                 new_lines.append(new_line[:MAX_SEQ_LEN])
                 new_line = new_line[MAX_SEQ_LEN:]
             if new_line != '':
                 new_lines.append(new_line)
+            new_line = ''
+        else:
+            new_line += piece
 
-        return new_lines
-
-
-
-    def get_line_combine(lines):
-        lines = list(filter(None, lines))
-        split_pattern = list("。！？：；」』’”")
-        new_lines = []
-        new_line = ''
-        for line in lines:
-            if len(line) == 1 and line[0] in split_pattern:
-                new_line += line
-            else:
-                if new_line != '':
-                    if len(new_line) <= MAX_SEQ_LEN:
-                        new_lines.append(new_line)
-                    else:
-                        new_lines.extend(split_long_sent(new_line))
-                    new_line = ''
-                new_line += line
+    if new_line != '':
+        while len(new_line) > MAX_SEQ_LEN:
+            new_lines.append(new_line[:MAX_SEQ_LEN])
+            new_line = new_line[MAX_SEQ_LEN:]
         if new_line != '':
-            if len(new_line) <= MAX_SEQ_LEN:
-                new_lines.append(new_line)
-            else:
-                new_lines.extend(split_long_sent(new_line))
-        return new_lines
+            new_lines.append(new_line)
+
+    return new_lines
+
+
+def get_line_combine(lines):
+    lines = list(filter(None, lines))
+    split_pattern = list("。！？：；」』’”")
+    new_lines = []
+    new_line = ''
+    for line in lines:
+        # “寔之政論言當世理亂，雖鼂錯之徒不能過也。”
+        if len(line) == 1 and line[0] in split_pattern:
+            new_line += line
+        else:
+            if new_line != '':
+                if len(new_line) <= MAX_SEQ_LEN:
+                    new_lines.append(new_line)
+                else:
+                    new_lines.extend(split_long_sent(new_line))
+                new_line = ''
+            new_line += line
+    if new_line != '':
+        if len(new_line) <= MAX_SEQ_LEN:
+            new_lines.append(new_line)
+        else:
+            new_lines.extend(split_long_sent(new_line))
+    return new_lines
+
+
+def main():
+    with open('dataset/gulian_txt/命名实体识别测试集922.txt', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
 
     new_lines = []
 
@@ -87,4 +89,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    line = '“寔之政論言當世理亂，雖鼂錯之徒不能過也。”'
+    line = re.split(r'([。：；！？」』])', line.strip())
+    get_line_combine(line)
